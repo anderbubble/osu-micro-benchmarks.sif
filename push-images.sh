c6bab32e4b5e2b5ec3ebf1fff6b10370dceabaf7
Jonathon Anderson <janderson@ciq.com>
2025-01-09 14:26:58 -0700

    Add "logging in" help text to push-images.sh
    Signed-off-by: Jonathon Anderson <janderson@ciq.com>

--- push-images.sh
+++ push-images.sh
@@ -5,6 +5,8 @@ base=$1
 if [ "${base}" == "" ]
 then
     echo "Example: $0 registry-1.docker.io/\$USER *.sif"
+    echo ""
+    echo "Logging in: apptainer registry login --username $USER oras://registry-1.docker.io"
     exit -1
 fi
 
